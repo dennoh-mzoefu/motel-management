d921b9a696435089f3dd6f955b447caaed9bf41f
Denis Gichana <denisgichana162@gmail.com>
2023-05-06 18:00:00 +0200

    add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="fixed top-0 left-0 h-full min-w-48 bg-slate-500 pt-4">
+      Sidebar
+    </div>
+  );
+}
+
+export default Sidebar;
