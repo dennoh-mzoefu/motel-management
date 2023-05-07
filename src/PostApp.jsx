2599e155e228adce690d9fa4b5003b2c3d31a945
Denis Gichana <denisgichana162@gmail.com>
2023-05-07 18:00:00 +0200

    add components

--- src/PostApp.jsx
+++ src/PostApp.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import MenuPage from "./pages/MenuPage";
+import { Route, Routes } from "react-router-dom";
+import StockPage from "./pages/StockPage";
+// import { auth } from "../utils/firebase";
 
 function PostApp() {
+  // const [user, loading] = useAuthState(auth);
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(0);
+
+  // const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 900) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
   return (
     <div>
       {" "}
-      <Sidebar />
-      <div className="ml-48">
+      <Sidebar
+        activeMenu={activeMenu}
+        setActiveMenu={setActiveMenu}
+        screenSize={screenSize}
+        setScreenSize={setScreenSize}
+        className=""
+      />
+      <div className="ml-52 bg-slate-100 min-h-screen">
         <Navbar />
-        Hotel Management
+        <Routes>
+          <Route path="/" element={<MenuPage />} />
+          <Route path="/stockManager" element={<StockPage />} />
+        </Routes>
       </div>
     </div>
   );
