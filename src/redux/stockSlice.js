5e9666b5711077a3f25de5c81215dcf7712845b8
Denis Gichana <denisgichana162@gmail.com>
2023-05-07 18:00:00 +0200

    stock slice

---
+++ src/redux/stockSlice.js
@@ -0,0 +1,41 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+import { addDoc, updateDoc } from "firebase/firestore";
+import { stockColRef } from "../utils/firebase";
+
+const initialState = {
+  stock: [],
+};
+
+export const stockSlice = createSlice({
+  name: "stock",
+  initialState,
+  reducers: {
+    fetchStock: (state, action) => {
+      return {
+        ...state,
+        stock: action.payload,
+      };
+    },
+    addStockItem: (state, action) => {
+      console.log(action.payload);
+      addDoc(stockColRef, {
+        quantity: action.payload.quantity,
+        name: action.payload.name,
+        buyingPrice: action.payload.buyingPrice,
+        sellingPrice: action.payload.sellingPrice,
+        time: new Date(),
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+    addQuantity: (state, action) => {
+      const addRef = doc(db, "stock", action.payload.id);
+      updateDoc(addRef, {
+        quantity: action.payload.quantity,
+      }).then(() => {});
+    },
+  },
+});
+
+export const { fetchStock, addStockItem, addQuantity } = stockSlice.actions;
+export default stockSlice.reducer;
