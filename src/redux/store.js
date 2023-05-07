bb827ccaf34e92b7298eb9f6b1c9964e8fb28af8
Denis Gichana <denisgichana162@gmail.com>
2023-05-07 18:00:00 +0200

    store cofig

---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+// import menuReducer from "./menuSlice";
+// import orderReducer from "./orderSlice";
+// import expenseReducer from "./expenseSlice";
+// import salesReducer from "./salesSlice";
+// import userReducer from "./userSlice";
+import stockReducer from "./stockSlice";
+
+export const store = configureStore({
+  reducer: {
+    // menu: menuReducer,
+    // order: orderReducer,
+    // expense: expenseReducer,
+    stock: stockReducer,
+    // sales: salesReducer,
+    // user: userReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
