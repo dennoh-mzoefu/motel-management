5267c8e2d9bd6179440fddd3258cd2ecbf3d915c
Denis Gichana <denisgichana162@gmail.com>
2023-05-07 18:00:00 +0200

    include preapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import PostApp from "./PostApp";
+import PreApp from "./PreApp";
 
 function App() {
   return (
     <div>
+      <PreApp className="w-0 h-0 " />
       <PostApp />
     </div>
   );
