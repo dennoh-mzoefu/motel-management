ef2a191cf50fd0c196151b708e38b506d64090bf
Denis Gichana <denisgichana162@gmail.com>
2023-05-09 18:00:00 +0200

    add order slice

---
+++ src/redux/orderSlice.js
@@ -0,0 +1,96 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+import { addDoc, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db, ordersColRef } from "../utils/firebase";
+const initialState = {
+  currentOrders: [],
+  orders: [],
+  order: {},
+  change: false,
+};
+
+export const orderSlice = createSlice({
+  name: "orders",
+  initialState,
+  reducers: {
+    changeListener: (state, action) => {
+      // state.change =
+    },
+    fetchOrders: (state, action) => {
+      console.log(current(state));
+      return {
+        ...state,
+        orders: action.payload,
+      };
+    },
+    addCurrentOrder: (state, action) => {
+      let a = state.currentOrders;
+      console.log(action.payload);
+      state.currentOrders.push(action.payload);
+    },
+    addOrder: (state, action) => {
+      console.log(action.payload);
+      addDoc(ordersColRef, {
+        quantity: action.payload.quantity,
+        name: action.payload.name,
+        price: action.payload.price,
+        isConfirmed: action.payload.isConfirmed,
+        isPaid: false,
+        isCancelled: action.payload.isCancelled,
+        time: new Date(),
+      }).then((res) => {
+        console.log(res);
+      });
+      return {
+        ...state,
+        currentOrders: state.currentOrders.filter(
+          (item) => item.orderId !== action.payload.orderId
+        ),
+      };
+    },
+    doneOrder: (state, action) => {
+      const doneDocRef = doc(db, "orders", action.payload);
+      updateDoc(doneDocRef, {
+        isConfirmed: true,
+      }).then(() => {});
+    },
+    unDoneOrder: (state, action) => {
+      const doneDocRef = doc(db, "orders", action.payload);
+      updateDoc(doneDocRef, {
+        isConfirmed: false,
+      }).then(() => {});
+    },
+    paidOrder: (state, action) => {
+      const doneDocRef = doc(db, "orders", action.payload);
+      updateDoc(doneDocRef, {
+        isPaid: true,
+      }).then(() => {});
+    },
+    deleteOrder: (state, action) => {
+      const docRef = doc(db, "orders", action.payload);
+      console.log("delete");
+      deleteDoc(docRef).then(() => {
+        console.log(action.payload);
+      });
+    },
+    deleteCurrentOrder: (state, action) => {
+      return {
+        ...state,
+        currentOrders: state.currentOrders.filter(
+          (item) => item.orderId !== action.payload
+        ),
+      };
+    },
+  },
+});
+
+export const {
+  fetchOrders,
+  addCurrentOrder,
+  addOrder,
+  deleteOrder,
+  deleteCurrentOrder,
+  doneOrder,
+  paidOrder,
+  unDoneOrder,
+} = orderSlice.actions;
+export default orderSlice.reducer;
