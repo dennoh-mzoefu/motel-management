55484c7366b9075b0cd4394adf8e85f16d5100e9
Denis Gichana <denisgichana162@gmail.com>
2023-05-10 18:00:00 +0200

    add store slice

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-// import menuReducer from "./menuSlice";
-// import orderReducer from "./orderSlice";
+import menuReducer from "./menuSlice";
+import orderReducer from "./orderSlice";
 // import expenseReducer from "./expenseSlice";
-// import salesReducer from "./salesSlice";
+import salesReducer from "./salesSlice";
 // import userReducer from "./userSlice";
 import stockReducer from "./stockSlice";
 
 export const store = configureStore({
   reducer: {
-    // menu: menuReducer,
-    // order: orderReducer,
+    menu: menuReducer,
+    order: orderReducer,
     // expense: expenseReducer,
     stock: stockReducer,
-    // sales: salesReducer,
+    sales: salesReducer,
     // user: userReducer,
   },
   middleware: (getDefaultMiddleware) =>
