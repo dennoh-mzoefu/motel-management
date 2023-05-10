9bb229ff1163b1ab28bb9c8f8000f2e96e30bf89
Denis Gichana <denisgichana162@gmail.com>
2023-05-10 18:00:00 +0200

    ad pre app

--- src/PreApp.jsx
+++ src/PreApp.jsx
@@ -2,15 +2,44 @@ import { onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useDispatch } from "react-redux";
-// import { stockColRef } from "./utils/firebase";
+import { fetchMenu } from "./redux/menuSlice";
+import { fetchOrders } from "./redux/orderSlice";
+import { fetchSales } from "./redux/salesSlice";
+// import { stockBeverageColRef } from "./utils/firebase";
 import { fetchStock } from "./redux/stockSlice";
-import { stockColRef } from "./utils/firebase";
+import {
+  menuFoodColRef,
+  stockBeverageColRef,
+  ordersColRef,
+  salesColRef,
+} from "./utils/firebase";
 function PreApp() {
   const dispatch = useDispatch();
   const [stock, setStock] = useState([]);
+  const [menu, setMenu] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [sales, setSales] = useState([]);
   useEffect(() => {
-    const unSubscribe = onSnapshot(stockColRef, (snapshot) => {
-      setStock(snapshot.docs.map((doc) => doc.data()));
+    const unSubscribe = onSnapshot(stockBeverageColRef, (snapshot) => {
+      setStock(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return () => unSubscribe();
+  }, []);
+  useEffect(() => {
+    const unSubscribe = onSnapshot(menuFoodColRef, (snapshot) => {
+      setMenu(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return () => unSubscribe();
+  }, []);
+  useEffect(() => {
+    const unSubscribe = onSnapshot(ordersColRef, (snapshot) => {
+      setOrders(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });
+    return () => unSubscribe();
+  }, []);
+  useEffect(() => {
+    const unSubscribe = onSnapshot(salesColRef, (snapshot) => {
+      setSales(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     });
     return () => unSubscribe();
   }, []);
@@ -19,6 +48,18 @@ function PreApp() {
     console.log({ stock });
     dispatch(fetchStock(stock));
   }, [stock]);
+  useEffect(() => {
+    console.log({ menu });
+    dispatch(fetchMenu(menu));
+  }, [menu]);
+  useEffect(() => {
+    console.log({ orders });
+    dispatch(fetchOrders(orders));
+  }, [orders]);
+  useEffect(() => {
+    console.log({ sales });
+    dispatch(fetchSales(sales));
+  }, [sales]);
   return <div>PreApp</div>;
 }
 
